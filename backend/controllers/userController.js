2c88006daccf9049318e09139ec7a5a24d1c449a
Prashant Gupta <prashant98132@gmail.com>
2023-10-12 16:10:54 +0545

    Logout function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -102,7 +102,22 @@ const loginUser = asyncHandler( async (req,res) => {
 
 });
 
+//create logout function
+
+const logout = asyncHandler(async (req,res) => {
+  res.cookie("token", "", {
+    path: "/",
+    httpOnly: true,
+    expires: new Date(0),
+    sameSite: "none",
+    secure: true,
+  });
+
+  return res.status(200).json({message: "Successfully Loged Out"});
+});
+
 module.exports = {
     registerUser,
     loginUser,
+    logout,
 }
